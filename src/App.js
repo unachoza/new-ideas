374e79a04817282412b7c748cc76e4ee1f44142a
arianna <unachoza@gmail.com>
2023-05-01 12:18:03 -0400

    using suspense and router displaying buttons to route to various pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, useState, useTransition } from "react";
+import IndexPage from "./Pages/IndexPage.js";
+import Page1 from "./Pages/Page1/Page1.js";
+import Page2 from "./Pages/Page2/Page2.js";
+import Page3 from "./Pages/Page3/Page3.js";
+import Page4 from "./Pages/Page4/Page4.js";
+import Layout from "./Layout.js";
+import LoadSpinner from "./Components/LoadingSpinner/LoadingSpinner.js";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<LoadSpinner />}>
+      <Router />
+    </Suspense>
   );
 }
 
-export default App;
+function Router() {
+  const [page, setPage] = useState("/");
+  const [isPending, startTransition] = useTransition();
+
+  function navigate(url) {
+    startTransition(() => {
+      setPage(url);
+    });
+  }
+
+  let content;
+  if (page === "/") {
+    content = <IndexPage navigate={navigate} />;
+  } else if (page === "/1") {
+    content = <Page1 navigate={navigate} />;
+  } else if (page === "/2") {
+    content = <Page2 navigate={navigate} />;
+  } else if (page === "/3") {
+    content = <Page3 navigate={navigate} />;
+  } else if (page === "/4") {
+    content = <Page4 navigate={navigate} />;
+  }
+  return <Layout isPending={isPending}>{content}</Layout>;
+}
+
+<Suspense fallback={<LoadSpinner />}>
+  <Router />
+</Suspense>;
