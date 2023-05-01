70bffeb2b0c4b64eee70f604afd653731c3300e6
arianna <unachoza@gmail.com>
2023-05-01 14:49:31 -0400

    drop down constants and style dropdown list items

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,21 +1,11 @@
 import { useState } from "react";
 import TextInput from "./Inputs/TextInput";
 import DropDownInput from "./DropDownInput/DropDownInput";
+import { TYPE, CATEGORIES } from "../../Constants/FormConstants";
 import "./Form.css";
 
 export default function Form() {
   const [formValues, setFormValues] = useState({ type: "", category: "", amount: "", date: "" });
-
-  const type = [
-    {
-      value: "Expense",
-      label: "Expense",
-    },
-    {
-      value: "Income",
-      label: "Income",
-    },
-  ];
 
   const handleInputChange = (e) => {
     const { name, value } = e.currentTarget;
@@ -33,7 +23,8 @@ export default function Form() {
           placeholder="enter spending here"
           setFormValues={handleInputChange}
         /> */}
-        <DropDownInput values={type} />
+        <DropDownInput values={TYPE} />
+        <DropDownInput values={CATEGORIES} />
       </div>
     </>
   );
---
+++ src/Constants/FormConstants.js
@@ -0,0 +1,49 @@
+export const TYPE = [
+  {
+    label: "Expense",
+    value: "Expense",
+  },
+  {
+    label: "Income",
+    value: "Income",
+  },
+];
+
+export const CATEGORIES = [
+  {
+    label: "Expense",
+    value: "Rent",
+  },
+  {
+    label: "Expense",
+    value: "Transportation",
+  },
+  {
+    label: "Expense",
+    value: "Food",
+  },
+  {
+    label: "Expense",
+    value: "Clothes",
+  },
+  {
+    label: "Expense",
+    value: "Entertainment",
+  },
+  {
+    label: "Expense",
+    value: "Shopping",
+  },
+  {
+    label: "Expense",
+    value: "Travel",
+  },
+  {
+    label: "Expense",
+    value: "Phone",
+  },
+  {
+    label: "Expense",
+    value: "Pets",
+  },
+];
