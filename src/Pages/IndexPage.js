d2dbc923f320a93af89110398a355d13213c8a4e
arianna <unachoza@gmail.com>
2023-05-02 09:13:41 -0400

    appdix and notes page

--- src/Pages/IndexPage.js
+++ src/Pages/IndexPage.js
@@ -8,7 +8,7 @@ export default function IndexPage({ navigate }) {
       <button onClick={() => navigate("/1")}>Page1</button>
       <button onClick={() => navigate("/2")}>Page2</button>
       <button onClick={() => navigate("/3")}>Page3</button>
-      <button onClick={() => navigate("/4")}>Page4</button>
+      <Button onClick={() => navigate("/4")} text="Appendix" />
     </div>
   );
 }
--- src/Pages/Page4/Page4.js
+++ src/Pages/Page4/Page4.js
@@ -1,8 +1,26 @@
-export default function Page2({ navigate }) {
+export default function Page4({ navigate }) {
   return (
     <div className="page">
-      <div>The Forth Page. And the last page.</div>
-      <button onClick={() => navigate("/")}>Back</button>
+      <div className="content">
+        <h2>New Hooks Used</h2>
+        <ul>
+          <li>useTransition</li>
+          <p>
+            Suspense and useTransition replace React.Router the previous way of routing to different pages in a
+            non-Single Page Application (the most common implementationof React)
+          </p>
+        </ul>
+        <h2>references</h2>
+        <ul>
+          <li>
+            <a href="https://www.scalablepath.com/react/react-18-release-features">18 Updates</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer">
+        <div>The Forth Page. And the last page.</div>
+        <button onClick={() => navigate("/")}>Back</button>
+      </div>
     </div>
   );
 }
