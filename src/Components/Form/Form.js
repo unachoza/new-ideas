b0cd45d6a29810bbf247c44ca6b47e3b0ebedeca
arianna <unachoza@gmail.com>
2023-05-03 14:51:57 -0400

    useMemo calculating history from local storage

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useState, useContext, useMemo } from "react";
 import TextInput from "./Inputs/TextInput";
 import DropDownInput from "./DropDownInput/DropDownInput";
-import Button from "../Button/Button";
+import Button from "../UI/Button/Button";
+import { TransactionContext } from "../../Context/TransactionContext";
 import { TRANSACTION, CATEGORIES } from "../../Constants/FormConstants";
 import "./Form.css";
 
 export default function Form() {
   const [formValues, setFormValues] = useState({ transaction: "", category: "", amount: "", date: "" });
   const [currentTotal, setCurrentTotal] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const { transactions, setTransactions } = useContext(TransactionContext);
 
+  const howMuchInHistoryIsTotal = () => {
+    let incomeSum = 0;
+    const filteredIncome = transactions.filter(({ transaction }) => transaction === "Income");
+    for (const tx of filteredIncome) {
+      incomeSum += parseInt(tx.amount);
+    }
+    return incomeSum;
+  };
+  const historyTotal = useMemo(() => {
+    return howMuchInHistoryIsTotal();
+  });
   const handleInputChange = (e) => {
     const { name, value } = e.currentTarget;
     setFormValues({
@@ -16,29 +31,60 @@ export default function Form() {
       [name]: value,
     });
   };
+  const doesFormHaveData = () => {
+    const { transaction, category, amount } = formValues;
+    if (transaction === "" || category === "" || amount === "") return false;
+  };
+  const createDefaultDate = () => {
+    if (formValues.date === "") {
+      const date = new Date();
+
+      let currentDay = String(date.getDate()).padStart(2, "0");
+
+      let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+
+      let currentYear = date.getFullYear();
+
+      // we will display the date as DD-MM-YYYY
+
+      let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+      setFormValues({
+        ...formValues,
+        date: currentDate,
+      });
+      return currentDate;
+    }
+  };
 
   const addTotal = (currentTotal, newValue) => {
+    setIncome((income) => income + newValue);
     currentTotal += newValue;
     setCurrentTotal(currentTotal);
     calcIncomePercent(currentTotal, newValue);
     return currentTotal;
   };
   const subtractTotal = (currentTotal, newValue) => {
+    setExpense((expense) => expense + newValue);
     currentTotal -= newValue;
     setCurrentTotal(currentTotal);
   };
 
   const calcIncomePercent = (currentTotal, newValue) => {
     const newValuePercent = newValue / currentTotal;
-    console.log({ newValuePercent });
+    return newValuePercent;
   };
 
   const handleSubmit = ({ amount, transaction }) => {
-    const numbericValue = parseInt(amount);
-    if (transaction === "Income") {
-      addTotal(currentTotal, numbericValue);
-    } else {
-      subtractTotal(currentTotal, numbericValue);
+    if (!doesFormHaveData()) {
+      createDefaultDate();
+      const numbericValue = parseInt(amount);
+      setTransactions([...transactions, formValues]);
+      if (transaction === "Income") {
+        addTotal(currentTotal, numbericValue);
+      } else {
+        subtractTotal(currentTotal, numbericValue);
+      }
     }
   };
   return (
@@ -56,7 +102,12 @@ export default function Form() {
         />
       </div>
       <Button text="Create Transaction" onClick={() => handleSubmit(formValues)} />
-      {currentTotal > 0 && <h1>Total: {currentTotal}</h1>}
+      <>
+        <h1>Income: {income}</h1>
+        <h1>Expense: {expense}</h1>
+        <h1>Total: {currentTotal}</h1>
+        <h1>historyTotal: {historyTotal}</h1>
+      </>
     </>
   );
 }
